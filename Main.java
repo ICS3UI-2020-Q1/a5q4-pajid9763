887c33842e4fe304b8e656cf5b663d9554ae6015
pajid9763 <pajid9763@wrdsb.ca>
2020-10-06 16:28:48 +0000

    finished interest rate program

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * program that calculates interest rate
+ * @author Darian
  */
 public class Main {
 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+
+  // ask the user to enter a starting balance 
+  System.out.println("Please enter a starting balance");
+  double balance = input.nextDouble();
+
+  // ask the user to enter a interest rate
+  System.out.println("Please enter your interest rate");
+  double rate = input.nextDouble();
+  double ratePercent = rate/100;
+
+  // ask the user how many years they are investing
+  System.out.println("Please enter the number of years you will invest for");
+  double year = input.nextDouble();
+
+  // calculate sum of interest rate
+  for (double i = 1; i <= year; i++){
+    double sum = balance * ratePercent;
+    balance += sum;
+  }
+    // tell the user their final balance
+    System.out.println("Your final balance would be $" + balance);
   }
 }
